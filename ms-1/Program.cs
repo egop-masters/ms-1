fe02cccfbe5b79be7384debf54ea9bce735906e0
Sinitsa Egor Yuryevich <esinitsa@ozon.ru>
2024-05-15 16:53:49 +0300

    add metrics

--- ms-1/Program.cs
+++ ms-1/Program.cs
@@ -1,6 +1,15 @@
+using Prometheus;
+
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello from ms-1");
+app.UseRouting();
+// Map the /metrics endpoint for Prometheus
+app.UseHttpMetrics(options => options.CaptureMetricsUrl = false); // Middleware for recording HTTP metrics
+app.MapMetrics(); // Endpoint for Prometheus to scrape metrics
+
+// Map the root endpoint
+app.MapGet("/test-endpoint", () => "Hello from ms-1");
 
 app.Run();
